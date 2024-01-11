d976484a9213455ccd847f881b754aa019bca4ea
rfuser <xiahaohua@rifeng.com>
2024-01-11 09:21:01 +0800

    'add'

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    outDir: 'dist'
   },
   base: './', // 这里更改打包相对绝对路径
   minify: true, // 是否压缩代码
